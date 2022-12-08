9272e6f1e70429789a1571288b6ad2ae8fb28efe
Jeffery Hebert <Jeffery.Hebert2@gmail.com>
2022-12-07 22:34:02 -0700

    fixed front end issues

--- ghi/app/src/ModelsForm.js
+++ ghi/app/src/ModelsForm.js
@@ -38,7 +38,7 @@ class ModelForm extends React.Component {
         event.preventDefault();
         const data = { ...this.state };
         delete data.manufacturers;
-
+        console.log(data)
         const ModelUrl = "http://localhost:8100/api/models/";
         const fetchConfig = {
             method: "post",
