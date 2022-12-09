765b1e0bdb7edd3ebeebe6d00da3ada9ea421370
Xue Yu <yxcassie@gmail.com>
2022-12-09 10:55:07 -0800

    nicer look anvbar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-opacity-75 bg-success">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,13 +12,13 @@ function Nav() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
             <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
             </li>
 
-            <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <li className="nav-item dropdown" >
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Manufacturers
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="manufacturers">Manufacturers List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="manufacturers/new">New Manufacturer</NavLink></li>
@@ -26,9 +26,9 @@ function Nav() {
             </li>
 
             <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Models
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="models">Models List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="models/new">New Model</NavLink></li>
@@ -36,9 +36,9 @@ function Nav() {
             </li>
 
             <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Automobiles
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="automobiles">Automobiles List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="automobiles/new">New Automobile</NavLink></li>
@@ -46,9 +46,9 @@ function Nav() {
             </li>
 
             <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Salespersons
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="salespersons">Salesperson List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="salespersons/new">New Salesperson</NavLink></li>
@@ -57,9 +57,9 @@ function Nav() {
             </li>
 
             <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Customers
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="customers">Customers List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="customers/new">New Customer</NavLink></li>
@@ -67,9 +67,9 @@ function Nav() {
             </li>
 
             <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Sales
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="sales">Sales List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="sales/new">New Sale</NavLink></li>
@@ -77,9 +77,9 @@ function Nav() {
             </li>
 
             <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Technicians
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="technicians">Technician List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="technicians/new">New Technician</NavLink></li>
@@ -87,9 +87,9 @@ function Nav() {
             </li>
 
             <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle bg-success" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className=" nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Appointments
-              </button>
+              </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
                 <li><NavLink className="dropdown-item" aria-current="page" to="appointments">Appointment List</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="appointments/new">New Appointment</NavLink></li>
